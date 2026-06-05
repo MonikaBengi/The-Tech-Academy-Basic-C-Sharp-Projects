de674c8e9b1a07b375e377f8d43bd45d9fa7b970
Monika Bengi <112941981+MonikaBengi@users.noreply.github.com>
2026-06-05 13:20:40 +0100

    Add files via upload
    Add Math And Comparison Operators Assignment – income comparison program

---
+++ MathAndComparisonOperators/MathAndComparisonOperators/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace MathAndComparisonOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Print program title
+            Console.WriteLine("Anonymous Income Comparison Program");
+
+            // Get Person 1 information
+            Console.WriteLine("Person 1");
+            Console.WriteLine("Hourly Rate?");
+            string hourlyRate1 = Console.ReadLine();
+
+            Console.WriteLine("Hours worked per week?");
+            string hours1 = Console.ReadLine();
+
+            // Calculate annual salary for Person 1
+            int salary1 = Convert.ToInt32(hourlyRate1) * Convert.ToInt32(hours1) * 52;
+
+            // Get Person 2 information
+            Console.WriteLine("Person 2");
+            Console.WriteLine("Hourly Rate?");
+            string hourlyRate2 = Console.ReadLine();
+
+            Console.WriteLine("Hours worked per week?");
+            string hours2 = Console.ReadLine();
+
+            // Calculate annual salary for Person 2
+            int salary2 = Convert.ToInt32(hourlyRate2) * Convert.ToInt32(hours2) * 52;
+
+            // Display salaries
+            Console.WriteLine("Annual salary of Person 1:");
+            Console.WriteLine(salary1);
+
+            Console.WriteLine("Annual salary of Person 2:");
+            Console.WriteLine(salary2);
+
+            // Compare salaries
+            Console.WriteLine("Person 1 makes more money than Person 2");
+
+            bool isMore = salary1 > salary2;
+
+            Console.WriteLine(isMore);
+
+            // Keep console window open
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MathAndComparisonOperators/MathAndComparisonOperators/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MathAndComparisonOperators")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("MathAndComparisonOperators")]
+[assembly: AssemblyCopyright("Copyright © HP 2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("144d15c2-8571-4bdc-9ef1-9ffa2d14eb90")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
