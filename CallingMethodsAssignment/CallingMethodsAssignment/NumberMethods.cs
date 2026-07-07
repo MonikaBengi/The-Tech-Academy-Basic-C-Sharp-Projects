9b6f6abb28d539dba062dc96cae2d2afaf85b130
Monika Bengi <112941981+MonikaBengi@users.noreply.github.com>
2026-07-07 13:45:33 +0100

    Add files via upload
    Complete Calling Methods Assignment

---
+++ CallingMethodsAssignment/CallingMethodsAssignment/NumberMethods.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CallingMethodsAssignment
+{
+    public class NumberMethods
+    {
+        // This method multiplies the number by 2.
+        public int Multiply(int number)
+        {
+            return number * 2;
+        }
+
+        // This method adds 20.
+        public int AddTwenty(int number)
+        {
+            return number + 20;
+        }
+
+        // This method subtracts 5.
+        public int SubtractFive(int number)
+        {
+            return number - 5;
+        }
+    }
+}
---
+++ CallingMethodsAssignment/CallingMethodsAssignment/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CallingMethodsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an object from the NumberMethods class.
+            NumberMethods methods = new NumberMethods();
+
+            // Ask the user for a number.
+            Console.WriteLine("Please enter a number:");
+
+            // Read the number.
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            // Call the first method.
+            Console.WriteLine("Multiply by 2: " + methods.Multiply(number));
+
+            // Call the second method.
+            Console.WriteLine("Add 20: " + methods.AddTwenty(number));
+
+            // Call the third method.
+            Console.WriteLine("Subtract 5: " + methods.SubtractFive(number));
+
+            Console.ReadLine();
+        }
+    }
+}
